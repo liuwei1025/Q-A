37e992a99c5271a737ef321578a1f25868703086
刘威 <liuwei89@jd.com>
2023-11-09 20:15:40 +0800

    fix: 处理目录结构问题

---
+++ .vitepress/dist/assets/chatGPT_index.md.e54ca5c2.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as t,Q as _}from"./chunks/framework.b48f7969.js";const m=JSON.parse('{"title":"问答记录","description":"","frontmatter":{},"headers":[],"relativePath":"chatGPT/index.md","filePath":"chatGPT/index.md"}'),o={name:"chatGPT/index.md"},r=_("",3),c=[r];function n(i,s,d,h,l,p){return a(),t("div",null,c)}const P=e(o,[["render",n]]);export{m as __pageData,P as default};
